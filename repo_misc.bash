32e7cc7e9b46e2d6e90d01f863b794ce265b1890
sammtcbn <sammtcbn@gmail.com>
2018-06-07 01:43:56 +0800

    add a script to download some source code

---
+++ repo_misc.bash
@@ -0,0 +1,24 @@
+#!/bin/bash
+# https://github.com/sammtcbn/dotfiles
+# Written by sammtcbn 2018.6.7
+
+function git_src() {
+    folder=$1
+    url=$2
+    echo "${url} ..."
+    if [ ! -d "${TOPDIR}/${folder}" ]; then
+        cd $TOPDIR || exit 1
+        git clone $url || exit 1
+    else
+        cd ${TOPDIR}/${folder} || exit 1
+        git pull || exit 1
+    fi
+}
+
+TOPDIR=~/repomisc
+mkdir -p ${TOPDIR}
+
+git_src samba         https://github.com/samba-team/samba.git
+git_src libwebsockets https://github.com/warmcat/libwebsockets.git 
+git_src opencv        https://github.com/opencv/opencv.git
+git_src linux         https://github.com/torvalds/linux.git

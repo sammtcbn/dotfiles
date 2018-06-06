38d4bc52a1bdf319904a1f5771c6e3cd2339309e
sammtcbn <sammtcbn@gmail.com>
2018-06-07 01:03:58 +0800

    install more packages for development

--- devenv_init.bash
+++ devenv_init.bash
@@ -1,5 +1,11 @@
 #!/bin/bash
+# https://github.com/sammtcbn/dotfiles
+# Written by sammtcbn 2018.6.7
 apt update
 apt -y install openssh-server
 apt -y install git
 apt -y install build-essential
+# build-essential consists of lib6-dev, gcc, g++, make, dpkg-dev
+apt -y install automake autoconf
+apt -y install libtool
+apt -y install cmake

c65259b94d10cc274172d7f6f4e13be1f3f42ba7
sammtcbn <sammtcbn@gmail.com>
2018-08-06 17:10:26 +0800

    add a script to download src of adv gitlab

---
+++ repo_adv.bash
@@ -0,0 +1,27 @@
+#!/bin/bash
+# https://github.com/sammtcbn/dotfiles
+# Written by sammtcbn 2018.8.6
+
+function git_src() {
+    folder=$1
+    url=$2
+    echo "${url} ..."
+    if [ ! -d "${TOPDIR}/${folder}" ]; then
+        cd $TOPDIR || exit 1
+        git clone $url || exit 1
+    else
+        cd ${TOPDIR}/${folder} || exit 1
+        git pull || exit 1
+    fi
+}
+
+TOPDIR=~/repo_adv
+mkdir -p ${TOPDIR}
+
+git_src ePack              http://advgitlab.eastasia.cloudapp.azure.com/EdgeSense/ePack.git
+
+git_src WAPI               http://advgitlab.eastasia.cloudapp.azure.com/EdgeSense/WAPI.git
+
+git_src EI-Connect         http://advgitlab.eastasia.cloudapp.azure.com/EI-Connect/EI-Connect.git
+
+git_src RMM-EI-Agent       http://advgitlab.eastasia.cloudapp.azure.com/EI-PaaS-RMM/RMM-EI-Agent.git

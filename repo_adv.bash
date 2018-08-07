9f9eb7d32ddc978bb86b3fd7af27ceb58b9ae619
sammtcbn <sammtcbn@gmail.com>
2018-08-07 16:22:17 +0800

    add parameter to compress repo_adv

--- repo_adv.bash
+++ repo_adv.bash
@@ -8,6 +8,24 @@ ADV_GITLAB_PW=
 ADV_SVN_ID=
 ADV_SVN_PW=
 
+PROJECTNAME=repo_adv
+
+while getopts "c" opt; do
+    case $opt in
+    c)
+       COMPRESS=1
+       ;;
+    esac
+done
+
+function do_compress() {
+    destfile=${PROJECTNAME}-$(date '+%Y%m%d').tar.gz
+    cd $TOPDIR/.. || exit 1
+    rm -f $destfile || exit 1
+    tar zcfv $destfile ${PROJECTNAME}
+    ls -la $destfile
+}
+
 function git_src() {
     folder=$1
     url=$2
@@ -21,38 +39,51 @@ function git_src() {
     fi
 }
 
-function svn_src() {
+function advgitlab_src() {
     folder=$1
     url=$2
     echo "${url} ..."
     if [ ! -d "${TOPDIR}/${folder}" ]; then
         cd $TOPDIR || exit 1
-        svn co --username $ADV_SVN_ID $url --password $ADV_SVN_PW || exit 1
+#        git clone $url || exit 1
+        git clone http://${ADV_GITLAB_ID}:${ADV_GITLAB_PW}@advgitlab.eastasia.cloudapp.azure.com/${url} || exit 1
+    else
+        cd ${TOPDIR}/${folder} || exit 1
+        git pull || exit 1
+    fi
+}
+
+
+function advsvn_src() {
+    folder=$1
+    url=$2
+    echo "${url} ..."
+    if [ ! -d "${TOPDIR}/${folder}" ]; then
+        cd $TOPDIR || exit 1
+        svn co --username $ADV_SVN_ID https://172.20.2.44/${url} --password $ADV_SVN_PW || exit 1
     else
        cd ${TOPDIR}/${folder} || exit 1
        svn up || exit 1
     fi
 }
 
-TOPDIR=~/repo_adv
+TOPDIR=~/${PROJECTNAME}
 mkdir -p ${TOPDIR}
 
 git_src APIGateway         https://github.com/ADVANTECH-Corp/APIGateway.git
-
 git_src WiseSnail          https://github.com/ADVANTECH-Corp/WiseSnail.git
 
-git_src ReyaxSol           http://${ADV_GITLAB_ID}@advgitlab.eastasia.cloudapp.azure.com/EdgeSense/ReyaxSol.git
-
-git_src ePack              http://${ADV_GITLAB_ID}@advgitlab.eastasia.cloudapp.azure.com/EdgeSense/ePack.git
-
-git_src MQTTBroker         http://${ADV_GITLAB_ID}@advgitlab.eastasia.cloudapp.azure.com/EdgeSense/MQTTBroker.git
-
-git_src WISE-3610_downlink http://${ADV_GITLAB_ID}@advgitlab.eastasia.cloudapp.azure.com/WISE-IoT/WISE-3610_downlink.git
-
-git_src WAPI               http://${ADV_GITLAB_ID}@advgitlab.eastasia.cloudapp.azure.com/EdgeSense/WAPI.git
+advgitlab_src ReyaxSol           EdgeSense/ReyaxSol.git
+advgitlab_src ePack              EdgeSense/ePack.git
+advgitlab_src MQTTBroker         EdgeSense/MQTTBroker.git
+advgitlab_src RMM-EI-Agent       EI-PaaS-RMM/RMM-EI-Agent.git
+advgitlab_src WAPI               EdgeSense/WAPI.git
+advgitlab_src WISE-3610_downlink WISE-IoT/WISE-3610_downlink.git
 
 git_src EI-Connect         http://advgitlab.eastasia.cloudapp.azure.com/EI-Connect/EI-Connect.git
 
-git_src RMM-EI-Agent       http://${ADV_GITLAB_ID}@advgitlab.eastasia.cloudapp.azure.com/EI-PaaS-RMM/RMM-EI-Agent.git
+advsvn_src CAgentIoT       svn/SUSIAccess/SA3.1/trunk/Agent/CAgentIoT
 
-svn_src CAgentIoT          https://172.20.2.44/svn/SUSIAccess/SA3.1/trunk/Agent/CAgentIoT
+if [ "${COMPRESS}" == 1 ]; then
+    do_compress
+fi

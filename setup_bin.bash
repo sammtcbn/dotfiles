72bb747691c026c34a11c05d8d6e2d16389fadbc
sammtcbn <sammtcbn@gmail.com>
2019-03-06 11:46:42 +0800

    move git script to another folder

--- setup_bin.bash
+++ setup_bin.bash
@@ -1,6 +1,7 @@
 #!/bin/bash
 # https://github.com/sammtcbn/dotfiles
-# Written by sammtcbn 2017.12.15
+# Written by sammtcbn 2019.3.6
 mkdir -p ~/bin/ || exit 1
 cp -f bin/* ~/bin/ || exit 1
+cp -f git_script/* ~/bin/ || exit 1
 echo "setup ok"

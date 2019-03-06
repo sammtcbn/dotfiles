ffd2afe1a1ab5201ec1bc9143265d61fe13c55f6
sammtcbn <sammtcbn@gmail.com>
2019-03-06 16:40:49 +0800

    use gdebi to install teamviewer

--- install_script/install_teamviewer.bash
+++ install_script/install_teamviewer.bash
@@ -7,20 +7,35 @@ if [ "$EUID" -ne 0 ]; then
     exit
 fi
 
-wget https://download.teamviewer.com/download/linux/teamviewer_amd64.deb || exit 1
-
-dpkg --add-architecture i386 || exit 1
-
 apt update || exit 1
 
-dpkg -i teamviewer_amd64.deb
+wget https://download.teamviewer.com/download/linux/teamviewer_amd64.deb || exit 1
 
-# install dependency
-apt-get -y install -f || exit 1
+function method_dpkg() {
+  dpkg --add-architecture i386 || exit 1
 
-# intall again
-dpkg -i teamviewer_amd64.deb || exit 1
+  dpkg -i teamviewer_amd64.deb
+
+  # install dependency
+  apt -y install -f || exit 1
+
+  # intall again
+  dpkg -i teamviewer_amd64.deb || exit 1
+}
+
+function method_gdebi() {
+  apt -y install gdebi-core
+  gdebi -n teamviewer_amd64.deb
+}
+
+function check_service_status() {
+  systemctl status teamviewerd
+}
+
+#method_dpkg
+method_gdebi
 
 rm -f teamviewer_amd64.deb || exit 1
 
-echo "done"
+systemctl enable teamviewerd || exit 1
+

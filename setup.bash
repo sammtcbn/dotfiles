af06c8da21e15b575a10cfafb37e337824044300
sammtcbn <sammtcbn@gmail.com>
2019-08-05 23:23:24 +0800

    add dircolors

--- setup.bash
+++ setup.bash
@@ -1,8 +1,9 @@
 #!/bin/bash
 # https://github.com/sammtcbn/dotfiles
-# Written by sammtcbn 2019.3.6
+# Written by sammtcbn 2019.8.5
 mkdir -p ~/bin/ || exit 1
 cp -f bin/* ~/bin/ || exit 1
 cp -f git_script/* ~/bin/ || exit 1
 cp -f dot/vimrc ~/.vimrc || exit 1
+cp -f dot/dircolors ~/.dircolors || exit 1
 echo "setup ok"

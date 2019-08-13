dd8282b1f578f3a9d4d0e219ef45025a1bab7609
sammtcbn <sammtcbn@gmail.com>
2019-08-13 18:00:16 +0800

    add git script for windows

---
+++ git_script_for_win/gfg1.bat
@@ -0,0 +1,4 @@
+git config user.name "Sam35.Lin"
+git config user.email "sam35.lin@advantech.com.tw"
+git config --local user.name
+git config --local user.email
---
+++ setup.bat
@@ -0,0 +1,2 @@
+xcopy /Y git_script_for_win\*.* C:\Users\%UserName%\AppData\Local\Microsoft\WindowsApps
+pause

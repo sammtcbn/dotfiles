9ab5e5a7ad017bd15089ea442f89d77e2de0a144
sammtcbn <sammtcbn@gmail.com>
2019-12-27 21:55:27 +0800

    update notes

--- install_script/uninstall_nodejs.bash
+++ install_script/uninstall_nodejs.bash
@@ -1,9 +1,10 @@
 #!/bin/bash
 # https://github.com/sammtcbn/dotfiles
-# Written by sammtcbn 2019.11.7
+# Written by sammtcbn 2019.12.27
 if [ "$EUID" -ne 0 ]; then
     echo "Please run as root"
     exit
 fi
 
-apt -y remove nodejs npm
+#apt -y remove nodejs npm
+apt -y purge nodejs npm

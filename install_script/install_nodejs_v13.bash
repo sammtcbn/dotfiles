e1bdbfe1c32669de587681aea849ef1f91072813
sammtcbn <sammtcbn@gmail.com>
2019-12-28 17:02:45 +0800

    update notes

--- install_script/install_nodejs_v13.bash
+++ install_script/install_nodejs_v13.bash
@@ -1,12 +1,17 @@
 #!/bin/bash
 # https://github.com/sammtcbn/dotfiles
-# Written by sammtcbn 2019.10.26
+# Written by sammtcbn 2019.12.28
 if [ "$EUID" -ne 0 ]; then
     echo "Please run as root"
     exit
 fi
 
+if ! [ -x "$(command -v curl)" ]; then
+    echo 'Error: curl is not installed' >&2
+    exit 1
+fi
+
 curl -sL https://deb.nodesource.com/setup_13.x | sudo -E bash -
-apt update
 apt -y install nodejs
-apt -y install npm 
+
+# npm is also installed
--- install_script/uninstall_nodejs.bash
+++ install_script/uninstall_nodejs.bash
@@ -1,10 +1,9 @@
 #!/bin/bash
 # https://github.com/sammtcbn/dotfiles
-# Written by sammtcbn 2019.12.27
+# Written by sammtcbn 2019.12.28
 if [ "$EUID" -ne 0 ]; then
     echo "Please run as root"
     exit
 fi
 
-#apt -y remove nodejs npm
-apt -y purge autoremove nodejs npm
+apt -y purge --auto-remove nodejs

f23c87552c8a89e944a187b9e1386e351d643ecf
sammtcbn <sammtcbn@gmail.com>
2020-03-19 01:32:48 +0800

    remove script for install node.js

--- install_script/uninstall_nodejs.bash
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# https://github.com/sammtcbn/dotfiles
-# Written by sammtcbn 2019.12.28
-if [ "$EUID" -ne 0 ]; then
-    echo "Please run as root"
-    exit
-fi
-
-apt -y purge --auto-remove nodejs

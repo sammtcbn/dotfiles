3c015f12ca97e279de9932c4c9a60e4c035bd9c1
sammtcbn <sammtcbn@gmail.com>
2023-05-16 13:52:08 +0800

    minor update

--- ubuntu-install-basic-sw.bash
+++ ubuntu-install-basic-sw.bash
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo apt-get -y update
-sudo apt-get -y install lsb-release openssh-server vim curl ncal samba sshpass git
+apt-get -y update
+apt-get -y install lsb-release openssh-server vim curl ncal samba sshpass
---
+++ ubuntu-install-dev-sw.bash
@@ -0,0 +1,13 @@
+#!/bin/bash
+apt-get -y update
+apt-get -y install git
+apt-get -y install build-essential
+# build-essential consists of lib6-dev, gcc, g++, make, dpkg-dev
+apt-get -y install automake autoconf
+apt-get -y install libtool
+apt-get -y install m4
+apt-get -y install cmake
+#apt -y install subversion
+
+apt-get -y install apt-file
+apt-file update

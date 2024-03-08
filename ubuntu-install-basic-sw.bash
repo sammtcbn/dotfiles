585f062ac18c1173d9c8c9a4c27f9863a8091d5b
sammtcbn <sammtcbn@gmail.com>
2024-03-08 16:08:05 +0800

    install some useful network util

--- ubuntu-install-basic-sw.bash
+++ ubuntu-install-basic-sw.bash
@@ -1,3 +1,6 @@
 #!/bin/bash
 apt-get -y update
-apt-get -y install lsb-release openssh-server vim curl ncal samba sshpass
+apt-get -y install lsb-release openssh-server vim curl ncal samba sshpass \
+                   apt-file dos2unix xmlstarlet \
+                   fio sysstat \
+                   nmap
